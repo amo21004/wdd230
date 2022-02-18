499697727ff404484ad2e5c8956ed4b162eab478
amo21004 <amo21004@byui.edu>
2022-02-18 17:00:23 +0530

    W07

--- chamber/js/common.js
+++ chamber/js/common.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  mobile_toggle();
+
+  date_related();
+
+  page_hits();
+
+  load_images();
+});
+
+function mobile_toggle() {
   document.querySelectorAll("header .toggle img").forEach((element) => {
     element.addEventListener("click", function () {
       document.querySelector("header .links").classList.toggle("open");
@@ -8,7 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+}
 
+function date_related() {
   let current_date = new Date();
 
   if (current_date.getDay() == 1 || current_date.getDay() == 2) {
@@ -24,4 +36,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.querySelector("footer .last-updated").innerHTML =
     document.lastModified;
-});
+}
+
+function page_hits() {
+  var page_hits = localStorage.getItem("discover-page-hits");
+
+  if (!page_hits) {
+    page_hits = 0;
+  }
+
+  page_hits++;
+
+  localStorage.setItem("discover-page-hits", page_hits);
+
+  document.querySelector("#page-hits").innerText = page_hits;
+}
+
+function load_images() {
+  const images = document.querySelectorAll("picture img[data-src]");
+
+  const load_image = (image) => {
+    // return in case data-src attribute is empty
+    if (!image.getAttribute("data-src")) {
+      return;
+    }
+
+    image.src = image.getAttribute("data-src");
+
+    image.removeAttribute("data-src");
+  };
+
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver(
+      (items, observer) => {
+        items.forEach((item) => {
+          if (item.isIntersecting) {
+            load_image(item.target);
+            observer.unobserve(item.target);
+          }
+        });
+      },
+      { rootMargin: "0px 0px 250px 0px" }
+    );
+
+    images.forEach((image) => {
+      observer.observe(image);
+    });
+  } else {
+    images.forEach((image) => {
+      load_image(image);
+    });
+  }
+}
